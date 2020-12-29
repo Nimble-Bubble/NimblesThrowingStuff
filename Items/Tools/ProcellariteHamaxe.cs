bd452778682e153a6ea452748673a6234b0ca3d7
Nimble-Bubble <44932098+Nimble-Bubble@users.noreply.github.com>
2020-12-28 20:12:44 -0500

    Procellarite Tools

---
+++ Items/Tools/ProcellariteHamaxe.cs
@@ -0,0 +1,37 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NimblesThrowingStuff.Items.Tools
+{
+	public class ProcellariteHamaxe : ModItem
+	{
+		public override void SetDefaults() 
+		{
+			item.damage = 80;
+			item.melee = true;
+			item.width = 54;
+			item.height = 52;
+			item.useTime = 5;
+			item.useAnimation = 12;
+			item.useStyle = 1;
+			item.knockBack = 8f;
+			item.value = Item.buyPrice(0, 50, 0, 0);
+			item.rare = 11;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+            item.axe = 40;
+            item.hammer = 125;
+            item.tileBoost += 6;
+		}
+        public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(mod.ItemType("ProcellariteBar"), 15);
+			recipe.AddTile(TileID.LunarCraftingStation);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ Items/Tools/ProcellaritePickaxe.cs
@@ -0,0 +1,36 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NimblesThrowingStuff.Items.Tools
+{
+	public class ProcellaritePickaxe : ModItem
+	{
+		public override void SetDefaults() 
+		{
+			item.damage = 100;
+			item.melee = true;
+			item.width = 48;
+			item.height = 48;
+			item.useTime = 4;
+			item.useAnimation = 10;
+			item.useStyle = 1;
+			item.knockBack = 6f;
+			item.value = Item.buyPrice(0, 50, 0, 0);
+			item.rare = 11;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+            item.pick = 300;
+            item.tileBoost += 6;
+		}
+        public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(mod.ItemType("ProcellariteBar"), 12);
+			recipe.AddTile(TileID.LunarCraftingStation);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
