f1135924e4254e0ffb250abd51d38d92ccf9fca1
Nimble-Bubble <44932098+Nimble-Bubble@users.noreply.github.com>
2021-04-07 13:26:32 -0400

    Aqua Blast now produces water dust

--- Projectiles/Summoning/AquaBlast.cs
+++ Projectiles/Summoning/AquaBlast.cs
@@ -62,7 +62,7 @@ namespace NimblesThrowingStuff.Projectiles.Summoning
         {
             Main.PlaySound(0, (int) projectile.position.X, (int) projectile.position.Y, 1, 1f, 0.0f);
                     for (int index = 0; index < 10; index++)
-                        Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 49,
+                        Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 33,
                             projectile.velocity.X * 0.1f, projectile.velocity.Y * 0.1f, 0, new Color(), 0.75f);
         }
     }
