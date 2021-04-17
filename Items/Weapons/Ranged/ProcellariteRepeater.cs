59ca83a27df2e98eaeeca45adf648c05206a087b
Nimble-Bubble <44932098+Nimble-Bubble@users.noreply.github.com>
2021-04-17 11:57:30 -0400

    Procellarite Repeater

---
+++ Items/Weapons/Ranged/ProcellariteRepeater.cs
@@ -0,0 +1,39 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using NimblesThrowingStuff.Items.Materials;
+
+namespace NimblesThrowingStuff.Items.Weapons.Ranged
+{
+	public class ProcellariteRepeater : ModItem
+	{
+		public override void SetDefaults()
+		{
+			item.damage = 102;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 5;
+			item.useAnimation = 6;
+			item.useStyle = 6;
+			item.value = Item.buyPrice(1, 0, 0, 0);
+			item.rare = 11;
+			item.noMelee = true;
+			item.useAmmo = AmmoID.Arrow;
+			item.UseSound = SoundID.Item5;
+			item.shoot = 1;
+            item.knockBack = 5f;
+			item.shootSpeed = 38f;
+			item.ranged = true;
+		}
+		public override void AddRecipes()
+		{
+			var recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ModContent.ItemType<ProcellariteBar>(), 10);
+			recipe.AddTile(TileID.LunarCraftingStation);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
