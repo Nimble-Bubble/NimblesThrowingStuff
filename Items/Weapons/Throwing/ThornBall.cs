5016ecba4b0890f78f327e1fc9500b8c1a982159
Nimble-Bubble <44932098+Nimble-Bubble@users.noreply.github.com>
2022-08-27 10:17:32 -0400

    Thorn Ball
    Meant to be a replacement of sorts for Spiky Balls

---
+++ Items/Weapons/Throwing/ThornBall.cs
@@ -0,0 +1,40 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NimblesThrowingStuff.Items.Weapons.Throwing
+{
+	public class ThornBall : ModItem
+	{
+		public override void SetDefaults() 
+		{
+			Item.damage = 18;
+			Item.DamageType = DamageClass.Throwing;
+			Item.width = 34;
+			Item.height = 34;
+			Item.useTime = 27;
+			Item.useAnimation = 27;
+			Item.useStyle = 1;
+			Item.knockBack = 2f;
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
+			Item.value = Item.buyPrice(0, 0, 1, 0);
+			Item.rare = 2;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+			Item.shoot = Mod.Find<ModProjectile>("ThornBallProj").Type;
+			Item.shootSpeed = 7f;
+            Item.consumable = true;
+            Item.maxStack = 9999;
+		}
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe(75);
+			recipe.AddIngredient(ItemID.JungleSpores, 3);
+			recipe.AddIngredient(ItemID.Stinger, 2);
+			recipe.AddTile(TileID.Anvils);
+			recipe.Register();
+		}
+	}
+}
---
+++ Projectiles/Throwing/ThornBallProj.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.Enums;
+
+namespace NimblesThrowingStuff.Projectiles.Throwing
+{
+	public class ThornBallProj : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 18;
+            Projectile.height = 18;
+            Projectile.tileCollide = true;
+            Projectile.maxPenetrate = 6;
+            Projectile.friendly = true;
+            Projectile.timeLeft = 3600;
+            Projectile.DamageType = DamageClass.Throwing;
+            Projectile.aiStyle = 14;
+            Projectile.penetrate = 6;
+        }
+        public override void OnHitNPC (NPC target, int damage, float knockback, bool crit)
+        {
+			target.AddBuff(BuffID.Poisoned, 300);
+        }
+    }
+}
