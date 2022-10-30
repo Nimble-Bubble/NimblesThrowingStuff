b1296532e7287d41e0fed490ea4f414a57030b75
Nimble-Bubble <44932098+Nimble-Bubble@users.noreply.github.com>
2022-10-29 21:53:21 -0400

    Blue Tail added
    post-Plantera upgrade to Red Tail

--- Items/Weapons/Melee/BlueTail.cs
+++ Items/Weapons/Melee/BlueTail.cs
@@ -9,15 +9,15 @@ using NimblesThrowingStuff.Items.Materials;
 
 namespace NimblesThrowingStuff.Items.Weapons.Melee
 {
-	public class RedTail : ModItem
+	public class BlueTail : ModItem
 	{
         public override void SetStaticDefaults()
         {
-			Tooltip.SetDefault("Made to resemble a Rathalos' tail. It's a bit small, but it spits flames."
+			Tooltip.SetDefault("A jeweled Lance at the forefront of fire weapon manufacturing technology."
 				+"\nRight click to spew flames from the tip of the lance.");
         }
         public override void SetDefaults() {
-			Item.damage = 26;
+			Item.damage = 72;
 			Item.useStyle = ItemUseStyleID.Shoot;
 			Item.useAnimation = 32;
 			Item.useTime = 32;
@@ -26,23 +26,23 @@ namespace NimblesThrowingStuff.Items.Weapons.Melee
 			Item.height = 20;
 			Item.noUseGraphic = true;
 			Item.noMelee = true;
-			Item.rare = ItemRarityID.Orange;
-			Item.value = Item.buyPrice(0, 15, 0, 0);
+			Item.rare = ItemRarityID.Lime;
+			Item.value = Item.buyPrice(0, 70, 0, 0);
             Item.DamageType = DamageClass.Melee;
 			Item.channel = true;
-            Item.shoot = ModContent.ProjectileType<RedTailProj>();
-            Item.shootSpeed = 10f;
+            Item.shoot = ModContent.ProjectileType<BlueTailProj>();
+            Item.shootSpeed = 12f;
 			Item.UseSound = SoundID.Item1;
 			Item.autoReuse = true;
 		}
 		public override void AddRecipes()
 		{
 			Recipe recipe = CreateRecipe();
-			recipe.AddIngredient(ModContent.ItemType<RedRathScale>(), 8);
-			recipe.AddIngredient(ItemID.BeeWax, 12);
-			recipe.AddIngredient(ItemID.Fireblossom, 10);
-			recipe.AddIngredient(ModContent.ItemType<RobustTusk>());
-			recipe.AddTile(TileID.Anvils);
+			recipe.AddIngredient(ModContent.ItemType<RedTail>());
+			recipe.AddIngredient(ItemID.SpectreBar, 12);
+			recipe.AddIngredient(ItemID.ShroomiteBar, 8);
+			recipe.AddIngredient(ItemID.LargeRuby);
+			recipe.AddTile(TileID.MythrilAnvil);
 			recipe.Register();
 		}
 	}
