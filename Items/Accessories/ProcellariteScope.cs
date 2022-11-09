f3e648b647ecaf0534e8d28f01e7c65c76901e9e
Nimble-Bubble <44932098+Nimble-Bubble@users.noreply.github.com>
2022-11-08 22:02:44 -0500

    Procellarite Scope added
    While it's not much of a stat-booster or visual spectacle, it can still be a useful tool, especially as an (indirect) upgrade to the vanilla Scope accessories.

---
+++ Items/Accessories/ProcellariteScope.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace NimblesThrowingStuff.Items.Accessories
+{
+    public class ProcellariteScope : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Allows you to move the camera regardless of what you are holding");
+        }
+        public override void SetDefaults()
+        {
+            Item.accessory = true;
+            Item.width = 22;
+            Item.height = 22;
+            Item.value = Item.buyPrice(0, 50, 0, 0);
+            Item.rare = ItemRarityID.Red;
+            Item.expert = true;
+        }
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.scope = true;
+        }
+    }
+}
--- Items/Consumables/MorilusTreasureBag.cs
+++ Items/Consumables/MorilusTreasureBag.cs
@@ -8,6 +8,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using NimblesThrowingStuff.NPCs.Morilus;
+using NimblesThrowingStuff.Items.Accessories;
 using NimblesThrowingStuff.Items.Accessories.Shields;
 using NimblesThrowingStuff.Items.Placeables.Blocks;
 using NimblesThrowingStuff.Items.Placeables.Furniture;
@@ -81,7 +82,8 @@ namespace NimblesThrowingStuff.Items.Consumables
         public override void ModifyItemLoot(ItemLoot itemLoot)
         {
             itemLoot.Add(ItemDropRule.CoinsBasedOnNPCValue(ModContent.NPCType<MorilusMain>()));
-            itemLoot.Add(ItemDropRule.Common(Mod.Find<ModItem>("CarpatusDefender").Type, 1));
+            itemLoot.Add(ItemDropRule.Common(Mod.Find<ModItem>("CarpatusDefender").Type, 3));
+            itemLoot.Add(ItemDropRule.Common(Mod.Find<ModItem>("ProcellariteScope").Type, 1));
             itemLoot.Add(ItemDropRule.OneFromOptions(1, Mod.Find<ModItem>("SkyseaSpinner").Type, Mod.Find<ModItem>("ProcellariteLongbow").Type, Mod.Find<ModItem>("GuardianStaff").Type, Mod.Find<ModItem>("StormShot").Type, Mod.Find<ModItem>("LacusDecapitator").Type));
             itemLoot.Add(ItemDropRule.Common(Mod.Find<ModItem>("SoulOfTrite").Type, 1, 16, 25));
             itemLoot.Add(ItemDropRule.Common(ModContent.ItemType<MorilusMask>(), 7));
