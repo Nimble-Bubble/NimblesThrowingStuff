03c558e99178643088f77aaba5eebb9d1a195e8b
Nimble-Bubble <44932098+Nimble-Bubble@users.noreply.github.com>
2022-11-17 22:09:58 -0500

    Fashionable Fragrance added
    "Poisonous spore cloud" is still just a regular spore cloud for now, but it will be changed soon

---
+++ Items/Weapons/Melee/FashionableFragrance.cs
@@ -0,0 +1,51 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using Microsoft.Xna.Framework;
+using System;
+using NimblesThrowingStuff.Projectiles.Melee;
+using NimblesThrowingStuff.Items.Materials;
+using Terraria.GameContent.Creative;
+
+namespace NimblesThrowingStuff.Items.Weapons.Melee
+{
+	public class FashionableFragrance : ModItem
+	{
+        public override void SetStaticDefaults()
+        {
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+			Tooltip.SetDefault("A lance modeled after a mythical plant. It matches its fashionable exterior with a pleasant odor."
+				+"\n...Pleasant enough to you, at least. Right click while attacking to fire a poisonous spore cloud.");
+        }
+        public override void SetDefaults() {
+			Item.damage = 46;
+			Item.useStyle = ItemUseStyleID.Shoot;
+			Item.useAnimation = 22;
+			Item.useTime = 22;
+			Item.knockBack = 5f;
+			Item.width = 20;
+			Item.height = 20;
+			Item.noUseGraphic = true;
+			Item.noMelee = true;
+			Item.rare = ItemRarityID.LightRed;
+			Item.value = Item.buyPrice(0, 17, 50, 0);
+            Item.DamageType = DamageClass.Melee;
+			Item.channel = true;
+            Item.shoot = ModContent.ProjectileType<FashionableFragranceProj>();
+            Item.shootSpeed = 14f;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+		}
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.TurtleShell, 1);
+			recipe.AddIngredient(ItemID.SoulofLight, 8);
+			recipe.AddIngredient(ItemID.SoulofNight, 8);
+			recipe.AddIngredient(ModContent.ItemType<Fragrance>());
+			recipe.AddTile(TileID.Anvils);
+			recipe.Register();
+		}
+	}
+}
---
+++ Items/Weapons/Ranged/HermitaurHuntingBow.cs
@@ -0,0 +1,50 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+using NimblesThrowingStuff.Items.Materials;
+
+namespace NimblesThrowingStuff.Items.Weapons.Ranged
+{
+	public class HermitaurHuntingbow : ModItem
+	{
+		public override void SetStaticDefaults()
+        {
+			DisplayName.SetDefault("Hermitaur Hunting Bow");
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+		public override void SetDefaults()
+		{
+			Item.damage = 20;
+			Item.width = 40;
+			Item.height = 40;
+			Item.useTime = 32;
+			Item.useAnimation = 32;
+			Item.useStyle = 5;
+			Item.value = Item.buyPrice(0, 5, 75, 0);
+			Item.rare = 1;
+			Item.noMelee = true;
+			Item.useAmmo = AmmoID.Arrow;
+			Item.UseSound = SoundID.Item5;
+			Item.shoot = 1;
+            Item.knockBack = 4f;
+			Item.shootSpeed = 9f;
+			Item.DamageType = DamageClass.Ranged;
+			Item.autoReuse = true;
+		}
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ModContent.ItemType<HermitaurShell>(), 12);
+			recipe.AddRecipeGroup(nameof(ItemID.GoldBar), 8);
+			recipe.AddTile(TileID.Anvils);
+			recipe.Register();
+		}
+        public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(0, -4);
+		}
+	}
+}
