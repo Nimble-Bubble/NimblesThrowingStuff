c040d6769a43bc2067fd25174b4490912800a470
Nimble-Bubble <44932098+Nimble-Bubble@users.noreply.github.com>
2022-12-09 22:39:38 -0500

    Added Fireball Brew
    To be edited later, hopefully

---
+++ Items/Weapons/Throwing/FireballBrew.cs
@@ -0,0 +1,46 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+
+namespace NimblesThrowingStuff.Items.Weapons.Throwing
+{
+	public class FireballBrew : ModItem
+	{
+		public override void SetStaticDefaults()
+        {
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+			Tooltip.SetDefault("When it breaks, it bursts into brilliant flames");
+		}
+		public override void SetDefaults() 
+		{
+			Item.damage = 31;
+			Item.DamageType = DamageClass.Throwing;
+			Item.width = 34;
+			Item.height = 34;
+			Item.useTime = 23;
+			Item.useAnimation = 23;
+			Item.useStyle = 1;
+			Item.knockBack = 4f;
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
+			Item.value = Item.buyPrice(0, 7, 50, 0);
+			Item.rare = 3;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+			Item.shoot = Mod.Find<ModProjectile>("FireballBrewProj").Type;
+			Item.shootSpeed = 6.5f;
+			Item.mana = 8;
+		}
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(Mod.Find<ModItem>("RedRathScale").Type, 12);
+			recipe.AddIngredient(ItemID.Bottle, 1);
+			recipe.AddIngredient(ItemID.HellstoneBar, 16);
+			recipe.AddTile(TileID.Anvils);
+			recipe.Register();
+		}
+	}
+}
---
+++ Projectiles/Throwing/FireballBrewProj.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.Enums;
+
+namespace NimblesThrowingStuff.Projectiles.Throwing
+{
+	public class FireballBrewProj: ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 18;
+            Projectile.height = 34;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 10;
+            Projectile.tileCollide = true;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Throwing;
+            Projectile.aiStyle = 68;
+            Projectile.penetrate = 1;
+        }
+        public override void AI()
+        {
+            var vector2 = new Vector2(20f, 20f);
+            Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, 6, Projectile.velocity.X * 0.1f, Projectile.velocity.Y * 0.1f, 0, new Color(), 0.75f);
+            Lighting.AddLight(Projectile.Center, Color.Orange.ToVector3() * 0.5f);
+        }
+        public override void Kill(int timeLeft) 
+        {
+            for (int f = 0; f < 30; f++)
+            {
+                int KindarathDust = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width * 2, Projectile.height * 2, 6, 0f, 0f, 100, default(Color), 3f);
+                Main.dust[KindarathDust].velocity *= 2f;
+            }
+            SoundEngine.PlaySound(SoundID.Shatter, Projectile.position);
+                var vector2 = new Vector2(20f, 20f);
+                for (int index = 0; index < 5; ++index)
+                Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, 6,
+                        Projectile.velocity.X * 0.1f, Projectile.velocity.Y * 0.1f, 0, new Color(), 0.75f);
+            for (int index1 = 0; index1 < 10; ++index1)
+                {
+                    int index2 = Dust.NewDust(Projectile.Center - vector2 / 2f, (int) vector2.X, (int) vector2.Y, 31, 0.0f,
+                        0.0f, 100, new Color(), 1.5f);
+                    Main.dust[index2].velocity *= 1.4f;
+                }
+
+                for (int index1 = 0; index1 < 20; ++index1)
+                {
+                    int index2 = Dust.NewDust(Projectile.Center - vector2 / 2f, (int) vector2.X, (int) vector2.Y, 102, 0.0f,
+                        0.0f, 100, new Color(), 2.5f);
+                    Main.dust[index2].noGravity = true;
+                    Main.dust[index2].velocity *= 5f;
+                    int index3 = Dust.NewDust(Projectile.Center - vector2 / 2f, (int) vector2.X, (int) vector2.Y, 102, 0.0f,
+                        0.0f, 100, new Color(), 1.5f);
+                    Main.dust[index3].velocity *= 3f;
+                }
+
+                if (Main.myPlayer == Projectile.owner)
+                {
+                    int type = Main.rand.Next(400, 403);
+                    for (int index = 0; index < 6; ++index)
+                    {
+                        var SpeedX =
+                            (float) (-(double) Projectile.velocity.X * (double) Main.rand.Next(20, 50) * 0.00999999977648258 +
+                                     (double) Main.rand.Next(-20, 21) * 0.400000005960464);
+                        var SpeedY =
+                            (float) (-(double) Math.Abs(Projectile.velocity.Y) * (double) Main.rand.Next(30, 50) *
+                                     0.00999999977648258 + (double) Main.rand.Next(-20, 5) * 0.400000005960464);
+                        int dothething = Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center.X + SpeedX, Projectile.Center.Y + SpeedY, SpeedX, SpeedY,
+                            type, (int) ((double) Projectile.damage * 0.5), 0.0f, Projectile.owner, 0.0f, 0.0f);
+                    Main.projectile[dothething].DamageType = DamageClass.Throwing;
+                }
+                }
+            }
+    }
+}
