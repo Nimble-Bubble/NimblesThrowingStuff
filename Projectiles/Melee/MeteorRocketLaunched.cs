2ff25855293d618b0296a64dc951b4be919ff6c7
Nimble-Bubble <44932098+Nimble-Bubble@users.noreply.github.com>
2023-11-28 13:29:14 -0500

    Meteor Rocket resprited
    It's not even fully implemented yet, and yet

---
+++ Projectiles/Melee/MeteorRocketLaunched.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.Enums;
+using NimblesThrowingStuff.Items.Weapons.Throwing;
+
+namespace NimblesThrowingStuff.Projectiles.Melee
+{
+	public class MeteorRocketLaunched: ModProjectile
+    {
+		public override void SetStaticDefaults()
+		{
+			// DisplayName.SetDefault("(thrown) Spiked Javelin");
+		}
+		public override void SetDefaults()
+		{
+			Projectile.width = 30;
+			Projectile.height = 30;
+			Projectile.usesLocalNPCImmunity = true;
+			Projectile.localNPCHitCooldown = 60;
+			Projectile.tileCollide = true;
+			Projectile.penetrate = 2;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Melee;
+			Projectile.extraUpdates = 0;
+			Projectile.timeLeft = 750;
+		}
+		public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough, ref Vector2 hitboxCenterFrac)
+		{
+			width = height = 20; 
+			return true;
+		}
+
+		public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+		{
+			if (targetHitbox.Width > 8 && targetHitbox.Height > 8)
+			{
+				targetHitbox.Inflate(-targetHitbox.Width / 8, -targetHitbox.Height / 8);
+			}
+			return projHitbox.Intersects(targetHitbox);
+		}
+        public bool IsStickingToTarget
+		{
+			get => Projectile.ai[0] == 1f;
+			set => Projectile.ai[0] = value ? 1f : 0f;
+		}
+		public int TargetWhoAmI
+		{
+			get => (int)Projectile.ai[1];
+			set => Projectile.ai[1] = value;
+		}
+
+		private const int MAX_STICKY_JAVELINS = 35; 
+		private readonly Point[] _stickingJavelins = new Point[MAX_STICKY_JAVELINS]; 
+
+		public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
+		{
+        if (Projectile.damage > 1)
+        {
+            Projectile.damage = 0;
+        }
+			IsStickingToTarget = true; 
+			TargetWhoAmI = target.whoAmI; 
+			Projectile.velocity =
+				(target.Center - Projectile.Center) *
+				0.75f; 
+			Projectile.netUpdate = true; 
+			UpdateStickyJavelins(target);
+		}
+		private void UpdateStickyJavelins(NPC target)
+		{
+			int currentJavelinIndex = 0; 
+
+			for (int i = 0; i < Main.maxProjectiles; i++) 
+			{
+				Projectile currentProjectile = Main.projectile[i];
+				if (i != Projectile.whoAmI 
+					&& currentProjectile.active 
+					&& currentProjectile.owner == Main.myPlayer 
+					&& currentProjectile.type == Projectile.type 
+					&& currentProjectile.ModProjectile is MeteorRocketLaunched daggerProjectile 
+					&& daggerProjectile.IsStickingToTarget 
+					&& daggerProjectile.TargetWhoAmI == target.whoAmI)
+				{
+
+					_stickingJavelins[currentJavelinIndex++] = new Point(i, currentProjectile.timeLeft); 
+					if (currentJavelinIndex >= _stickingJavelins.Length)  
+						break;
+				}
+			}
+			if (currentJavelinIndex >= MAX_STICKY_JAVELINS)
+			{
+				int oldJavelinIndex = 0;
+				for (int i = 1; i < MAX_STICKY_JAVELINS; i++)
+				{
+					if (_stickingJavelins[i].Y < _stickingJavelins[oldJavelinIndex].Y)
+					{
+						oldJavelinIndex = i; 
+					}
+				}
+				Main.projectile[_stickingJavelins[oldJavelinIndex].X].Kill();
+			}
+		}
+		private const int MAX_TICKS = 60;
+		int deathCount = 0;
+		public override void AI()
+		{
+			if (IsStickingToTarget) StickyAI();
+			else NormalAI();
+		}
+
+		private void NormalAI()
+		{
+			TargetWhoAmI++;
+			if (TargetWhoAmI >= MAX_TICKS)
+			{
+				const float velXmult = 0.98f; 
+				const float velYmult = 0.14f; 
+				TargetWhoAmI = MAX_TICKS; 
+				Projectile.velocity.X *= velXmult;
+				Projectile.velocity.Y += velYmult;
+			}
+			Projectile.rotation =
+				Projectile.velocity.ToRotation() +
+				MathHelper.ToRadians(90f);
+		}
+
+		private void StickyAI()
+		{
+			Projectile.ignoreWater = true; 
+            Projectile.alpha += 1;
+			Projectile.tileCollide = false; 
+			const int aiFactor = 3; 
+			Projectile.localAI[0] += 1f;
+			bool hitEffect = Projectile.localAI[0] % 30f == 0f;
+			int projTargetIndex = (int)TargetWhoAmI;
+			if (Projectile.localAI[0] >= 60 * aiFactor || projTargetIndex < 0 || projTargetIndex >= 200)
+			{ 
+				Projectile.Kill();
+			}
+			else if (Main.npc[projTargetIndex].active && !Main.npc[projTargetIndex].dontTakeDamage)
+			{ 
+				Projectile.Center = Main.npc[projTargetIndex].Center - Projectile.velocity * 2f;
+				Projectile.gfxOffY = Main.npc[projTargetIndex].gfxOffY;
+				if (hitEffect)
+				{ 
+					Main.npc[projTargetIndex].HitEffect(0, 1.0);
+				}
+			}
+			else
+			{ 
+				Projectile.Kill();
+			}
+		}
+        public override void OnKill(int timeLeft)
+        {
+            Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.velocity, ModContent.ProjectileType<PecoTeepeeShell>(), Projectile.damage * 1, 12f, Projectile.owner);
+            for (int wd = 0; wd < 25; wd++)
+        {
+            Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, DustID.Torch,
+                            Projectile.velocity.X * 0.1f, Projectile.velocity.Y * 0.1f, 0, new Color(), 0.75f);
+                            }
+        }
+    }
+}
