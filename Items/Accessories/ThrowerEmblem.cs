3e99965c6dfbb8027d9ef766835d11a5d75bbf5b
Nimble-Bubble <44932098+Nimble-Bubble@users.noreply.github.com>
2025-06-28 21:13:50 -0400

    Added recipes involving Thrower Emblem

--- Items/Accessories/ThrowerEmblem.cs
+++ Items/Accessories/ThrowerEmblem.cs
@@ -34,12 +34,45 @@ namespace NimblesThrowingStuff.Items.Accessories
         public override void AddRecipes()
 		{
 			Recipe recipe = Recipe.Create(935, 1);
-			recipe.AddIngredient(this); //modded materials
+			recipe.AddIngredient(this);
             recipe.AddIngredient(547, 5);
 			recipe.AddIngredient(548, 5);
             recipe.AddIngredient(549, 5);
 			recipe.AddTile(114);
 			recipe.Register();
-		}
+            //Some mods let you swap the emblems for each other, and I don't know how to do Shimmer recipes yet, so this is my solution for the time being
+            recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.SorcererEmblem);
+			recipe.AddTile(114);
+			recipe.Register();
+            recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.WarriorEmblem);
+			recipe.AddTile(114);
+			recipe.Register();
+            recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.RangerEmblem);
+			recipe.AddTile(114);
+			recipe.Register();
+            recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.SummonerEmblem);
+			recipe.AddTile(114);
+			recipe.Register();
+            recipe = Recipe.Create(ItemID.SorcererEmblem, 1);
+			recipe.AddIngredient(this);
+			recipe.AddTile(114);
+			recipe.Register();
+            recipe = Recipe.Create(ItemID.WarriorEmblem, 1);
+			recipe.AddIngredient(this);
+			recipe.AddTile(114);
+			recipe.Register();
+            recipe = Recipe.Create(ItemID.RangerEmblem, 1);
+			recipe.AddIngredient(this);
+			recipe.AddTile(114);
+			recipe.Register();
+            recipe = Recipe.Create(ItemID.SummonerEmblem, 1);
+			recipe.AddIngredient(this);
+			recipe.AddTile(114);
+			recipe.Register();
+        }
     }
 }
